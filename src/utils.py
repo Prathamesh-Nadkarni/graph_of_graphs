6e4bc3835bb4927ee7e7390248e8618800f226b6
John Lay <jlordo112@gmail.com>
2024-04-14 01:31:35 -0500

    typos

--- src/utils.py
+++ src/utils.py
@@ -6,20 +6,16 @@ def parse(filename):
 def synth_age_demographics(low, high, step):
     n = (high-low)//step
     ranges = list(f'{low + i*step}-{low + (i+1)*step}' for i in range(n))
-    n = len(ranges)
-
+    
     NUM_FRIENDS = 30
-
-    out = {}
+    age_data = {}
     for sample in range(3):
         friends = np.clip(np.round(np.random.normal(loc=NUM_FRIENDS, scale=NUM_FRIENDS//3)), 0, NUM_FRIENDS*3).astype(int)
         range_idx = np.random.randint(0, n)
         friends_idx = np.clip(np.round(np.random.normal(loc=range_idx, scale=1.5, size=friends)), 0, n-1).astype(int)
         friends = list(ranges[f_idx] for f_idx in friends_idx)
-        out[sample] = (ranges[range_idx], friends)
-    return out
-
-print(synth_age_demographics(20, 50, 5))
+        age_data[sample] = (ranges[range_idx], friends)
+    return age_data
 
 
 
