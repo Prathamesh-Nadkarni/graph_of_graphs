46361e753e9cbd00c370bd84c5053f3880407968
Wyatt Mayor <97546430+WyattMayor@users.noreply.github.com>
2024-04-30 13:00:39 -0500

    P - D

--- src/main.py
+++ src/main.py
@@ -4,16 +4,17 @@ import networkx as nx
 import py4cytoscape as p4c
 
 def main():
-    product_data = utils.parse_json(r"Product Network\Product_Edges.json")
-    demographic_data = utils.parse_json(r'data\age_friends.json')
+    product_data = utils.parse_json(r"src\Product Network\Product_Edges.json")
+    demographic_data = utils.parse_json(r'src\data\age_friends.json')
     social_data = None
 
-    p_to_d_data = r'src\ConnectionProductDemoGraphic\data\retail_data.csv'
-    d_to_s_data = utils.parse_json(r'data\age_to_sm.json')
+    p_to_d_data = r'src\data\retail_data.csv'
+    d_to_s_data = utils.parse_json(r'src\data\age_to_sm.json')
 
     m = model.Model(product_data, demographic_data, None, p_to_d_data, d_to_s_data)
     m.visualize_cytoscape(m.P)
     m.visualize_cytoscape(m.D, add_weights=False)
+    m.visualize_cytoscape(m.P_to_D, add_weights=True)
     m.visualize_cytoscape(m.D_to_S, add_weights=True)
 
     #print(m.query("toothpaste"))
