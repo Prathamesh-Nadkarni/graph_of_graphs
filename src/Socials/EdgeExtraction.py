4a802ee365b5964a4af3a8b081408d503990df8c
Prathamesh Nadkarni <pyn2@illinois.edu>
2024-04-30 21:04:23 -0500

    Correcting bugs

--- src/Socials/EdgeExtraction.py
+++ src/Socials/EdgeExtraction.py
@@ -2,18 +2,17 @@ import pandas as pd
 import json
 
 Probability_csv = 'probabilities.csv'
-df = pd.read_csv(Probability_csv, index_col='age')  # Assuming 'age' is the column to use as an index
+df = pd.read_csv(Probability_csv, index_col='age')  
 
 def clean_numeric(value):
-    if isinstance(value, str):  # Check if the value is a string
-        value = value.strip('.')  # Strip trailing period
+    if isinstance(value, str):  
+        value = value.strip('.')  
         try:
-            return pd.to_numeric(value)  # Convert to numeric type
+            return pd.to_numeric(value)  
         except ValueError:
-            return pd.NA  # Return missing value if conversion fails
+            return pd.NA  
     else:
-        return value  # Return the value unchanged if it's not a string
-    
+        return value  
 
 categories = ['TikTok', 'Twitter', 'Instagram', 'Facebook', 'YouTube']
 category_dfs = {}
@@ -23,29 +22,23 @@ for column in categories:
 
 df = df.dropna()
 
-# Initialize dictionary to store edge weights
 EdgeDict = {}
 
-# Iterate over each category to calculate edge weights
 for category in categories:
-    category_df = df.copy()  # Make a copy of the DataFrame
+    category_df = df.copy()  
     
-    # Calculate edge weights for each category
     EdgePercentage = {}
     for x in categories:
         if x != category:
             category_df[f'{category}-{x}'] = df[category] / df[x]
-            EdgePercentage[x] = category_df[f'{category}-{x}'].mean().round(3)  # Calculate average edge weight and round to 3 decimal places
+            EdgePercentage[x] = category_df[f'{category}-{x}'].mean().round(3)  
     
-    # Remove NaN values from the dictionary
-    EdgePercentage = {k: v for k, v in EdgePercentage.items() if not pd.isna(v)}
+    total_weight = sum(EdgePercentage.values())
+    normalized_weights = {k: (v / total_weight).round(3) for k, v in EdgePercentage.items()}
     
-    # Add edge weights to EdgeDict
-    EdgeDict[category] = EdgePercentage
+    EdgeDict[category] = normalized_weights
 
-# Save edge weights to a JSON file
-with open('Social_Edges.json', 'w') as f:
+with open('Demograph_Edges.json', 'w') as f:
     json.dump(EdgeDict, f, indent=4)
 
-# Print the edge weights
 print(json.dumps(EdgeDict, indent=4))
